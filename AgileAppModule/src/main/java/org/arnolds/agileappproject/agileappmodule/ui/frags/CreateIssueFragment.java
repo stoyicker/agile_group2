3342021ff697bd3e21e3430cb806fd4aa0192993
Magnus Sjoqvist <magnus.b.j.sjoqvist@gmail.com>
2014-04-10 11:24:11 +0200

    Added a progessbar to the ui

--- AgileAppModule/src/main/java/org/arnolds/agileappproject/agileappmodule/ui/frags/CreateIssueFragment.java
+++ AgileAppModule/src/main/java/org/arnolds/agileappproject/agileappmodule/ui/frags/CreateIssueFragment.java
@@ -5,16 +5,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.arnolds.agileappproject.agileappmodule.R;
 import org.arnolds.agileappproject.agileappmodule.git.GitHubBroker;
+import org.arnolds.agileappproject.agileappmodule.git.GitHubBrokerListener;
 import org.arnolds.agileappproject.agileappmodule.git.IGitHubBroker;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
 
+import java.util.Collection;
+
+import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
 
 
 public class CreateIssueFragment extends Fragment {
+
+    private View view;
+    private EditText editTextTitle;
+    private EditText editTextComment;
 
     public CreateIssueFragment() {
         // Required empty public constructor
@@ -29,7 +42,7 @@ public class CreateIssueFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_create_issue, container, false);
+        view = inflater.inflate(R.layout.fragment_create_issue, container, false);
         Button createIssueButton = (Button) view.findViewById(R.id.create_issue_button);
         createIssueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,19 +55,113 @@ public class CreateIssueFragment extends Fragment {
     }
 
     private void submit() {
-        EditText editTextTitle = (EditText) getView().findViewById(R.id.create_issue_title);
-        String title = editTextTitle.getText().toString();
 
-        EditText editTextComment = (EditText) getView().findViewById(R.id.create_issue_comment);
-        String comment = editTextComment.getText().toString();
+        show_progress(true);
+
+        final Button createIssueButton = (Button) view.findViewById(R.id.create_issue_button);
+        createIssueButton.setVisibility(View.INVISIBLE);
+
+        editTextTitle = (EditText) getView().findViewById(R.id.create_issue_title);
+        final String title = editTextTitle.getText().toString();
+
+        editTextComment = (EditText) getView().findViewById(R.id.create_issue_comment);
+        final String comment = editTextComment.getText().toString();
+
+
+        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(
+                getActivity().INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editTextTitle.getWindowToken(), 0);
 
         //Checks that the title is not all spaces or empty.
         if (!title.trim().isEmpty()){
+
+
+
+            final GitHubBrokerListener listener = new GitHubBrokerListener() {
+                @Override
+                public void onIssueCreation(boolean result, GHIssue issue) {
+                    super.onIssueCreation(result, issue);
+                    getActivity().runOnUiThread(new Runnable() {
+                        public void run() {
+                            show_progress(false);
+                            show_result();
+                        }
+                    });
+
+                }
+
+                @Override
+                public void onAllReposRetrieved(boolean success, Collection<GHRepository> repos) {
+                    super.onAllReposRetrieved(success, repos);
+                    try {
+                        GitHubBroker.getInstance().selectRepo(repos.iterator().next(), this);
+                    } catch (GitHubBroker.AlreadyNotConnectedException e) {
+                        e.printStackTrace();
+                    } catch (GitHubBroker.NullArgumentException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onRepoSelected(boolean result) {
+                    super.onRepoSelected(result);
+                    try {
+                        GitHubBroker.getInstance().createIssue(title, comment, null, this);
+                    } catch (GitHubBroker.AlreadyNotConnectedException e) {
+                        e.printStackTrace();
+                    } catch (GitHubBroker.RepositoryNotSelectedException e) {
+                        e.printStackTrace();
+                    } catch (GitHubBroker.NullArgumentException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
             IGitHubBroker gitHubBroker = GitHubBroker.getInstance();
+            try {
+                gitHubBroker.getAllRepos(listener);
+            } catch (GitHubBroker.AlreadyNotConnectedException e) {
+                e.printStackTrace();
+            }
+
+
+
         }
         else{
             editTextTitle.setError(getString(R.string.error_no_title));
         }
+
+    }
+
+    private void show_progress(boolean show) {
+        SmoothProgressBar progressBar = (SmoothProgressBar) view.findViewById(R.id.create_issue_progess_bar);
+        TextView creatingIssueText = (TextView) view.findViewById(R.id.create_issue_creating_issue_text);
+        Button createIssueButton = (Button) view.findViewById(R.id.create_issue_button);
+
+        if (show){
+            progressBar.setVisibility(View.VISIBLE);
+            creatingIssueText.setVisibility(View.VISIBLE);
+            createIssueButton.setVisibility(View.INVISIBLE);
+        }
+        else if (!show){
+            progressBar.setVisibility(View.INVISIBLE);
+            creatingIssueText.setVisibility(View.INVISIBLE);
+            createIssueButton.setVisibility(View.VISIBLE);
+        }
+    }
+    private void show_result(){
+        //TODO use tags instead
+        Button createIssueButton = (Button) view.findViewById(R.id.create_issue_button);
+        TextView createdIssueTitle = (TextView) view.findViewById(R.id.issue_title_text);
+        TextView createdIssueText = (TextView) view.findViewById(R.id.created_issue_text);
+
+        createdIssueText.setVisibility(View.VISIBLE);
+        createdIssueTitle.setVisibility(View.VISIBLE);
+        createdIssueTitle.setText(editTextTitle.getText().toString());
+
+        editTextComment.setVisibility(View.INVISIBLE);
+        editTextTitle.setVisibility(View.INVISIBLE);
+        createIssueButton.setVisibility(View.INVISIBLE);
 
     }
 
