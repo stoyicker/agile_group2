72e828fb06e0d0a1dab1cb441741df4cd2b20b35
Vectovox <robert-kaufmann@live.com>
2014-05-06 12:30:32 +0200

    Polished the issue list and added more information on every issue.

--- AgileAppModule/src/main/java/org/arnolds/agileappproject/agileappmodule/ui/frags/ListIssuesFragment.java
+++ AgileAppModule/src/main/java/org/arnolds/agileappproject/agileappmodule/ui/frags/ListIssuesFragment.java
@@ -7,8 +7,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,11 +22,12 @@ import org.arnolds.agileappproject.agileappmodule.git.IGitHubBrokerListener;
 import org.arnolds.agileappproject.agileappmodule.ui.activities.DrawerLayoutFragmentActivity;
 import org.kohsuke.github.GHIssue;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ListIssuesFragment extends ArnoldSupportFragment {
+public class ListIssuesFragment extends ArnoldSupportFragment implements AdapterView.OnItemClickListener {
 
     private static final long ISSUES_POLL_INTERVAL_MILLIS = 2000;
     private final static int MENU_INDEX = 2;
@@ -33,6 +37,13 @@ public class ListIssuesFragment extends ArnoldSupportFragment {
 
     public ListIssuesFragment() {
         super(MENU_INDEX);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        listAdapter.getItem(i);
+        IssuesListAdapter.ViewHolder vh = (IssuesListAdapter.ViewHolder) view.getTag();
+        vh.toggleExpanded();
     }
 
     @Override
@@ -115,32 +126,137 @@ public class ListIssuesFragment extends ArnoldSupportFragment {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder viewHolder;
+            final ViewHolder viewHolder;
             if (convertView == null) {
                 convertView = ((LayoutInflater) getActivity().getApplicationContext()
                         .getSystemService(Context.LAYOUT_INFLATER_SERVICE))
                         .inflate(R.layout.list_item_issue, null);
                 viewHolder = new ViewHolder();
                 viewHolder.setTitleView((TextView) convertView.findViewById(R.id.title_issue));
+                viewHolder.setCreatedView((TextView) convertView.findViewById(R.id.issue_created));
+                viewHolder.setCreatorView((TextView) convertView.findViewById(R.id.issue_creator));
+                viewHolder.setBodyView((TextView) convertView.findViewById(R.id.issue_body));
+                viewHolder.setExpandPanel((RelativeLayout) convertView.findViewById(R.id.issue_expand_panel));
+                viewHolder.setExpandIcon((ImageView) convertView.findViewById(R.id.issue_expander_icon));
+                viewHolder.setAssigneeView((TextView) convertView.findViewById(R.id.issue_assignee));
                 convertView.setTag(viewHolder);
+
+                viewHolder.setExpanded(false);
             }
             else {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
-            GHIssue issue = getItem(position);
-            viewHolder.getTitleView().setText(issue.getTitle());
+
+            if (position % 2 == 0) {
+                convertView.findViewById(R.id.issue_item_fragment)
+                        .setBackgroundColor(getResources().getColor(R.color.list_row_background1));
+            }
+            else {
+                convertView.findViewById(R.id.issue_item_fragment)
+                        .setBackgroundColor(getResources().getColor(R.color.list_row_background2));
+            }
+
+            final GHIssue issue = getItem(position);
+
+            String title = "#" + issue.getNumber() + ": " + issue.getTitle();
+            String creator = "Created by: " + issue.getUser().getLogin();
+            String date = "ERROR";
+            if(issue.getClosedAt() != null){
+                date = "Closed at: " + issue.getClosedAt().toString();
+            } else if(issue.getUpdatedAt() != null){
+                date = "Updated at: " + issue.getUpdatedAt().toString();
+            } else if(issue.getCreatedAt() != null) {
+                date = "Created at: " + issue.getCreatedAt().toString();
+            }
+
+            String body = issue.getBody();
+            if(body.length() == 0){
+                body = "<No message>";
+            }
+            viewHolder.getBodyView().setText(body);
+
+            if(issue.getAssignee() == null){
+                viewHolder.getAssigneeView().setVisibility(TextView.GONE);
+            } else {
+                viewHolder.getAssigneeView().setVisibility(TextView.VISIBLE);
+                viewHolder.getAssigneeView().setText(issue.getAssignee().getLogin());
+            }
+
+            viewHolder.getTitleView().setText(title);
+            viewHolder.getCreatorView().setText(creator);
+            viewHolder.getCreatedView().setText(date);
+
             return convertView;
         }
 
         private final class ViewHolder {
             private TextView titleView;
+            private TextView createdView;
+            private TextView creatorView;
+            private TextView assigneeView;
+            private TextView bodyView;
+            private RelativeLayout expandPanel;
+            private ImageView expandIcon;
+
+            private boolean expanded = false;
+
+            public void setExpanded(boolean expanded){
+                if(this.expandPanel == null){
+                    throw new IllegalStateException("Expand panel not set");
+                }
+                if(this.expandIcon == null){
+                    throw new IllegalStateException("Expand icon not set");
+                }
+                this.expanded = expanded;
+                this.expandPanel.setVisibility(this.expanded ? RelativeLayout.VISIBLE : RelativeLayout.GONE);
+                this.expandIcon.setImageResource(this.expanded ? R.drawable.expander_ic_maximized
+                        : R.drawable.expander_ic_minimized);
+            }
+
+            public void toggleExpanded(){
+                this.setExpanded(!this.expanded);
+            }
+
+            public TextView getBodyView() {
+                return this.bodyView;
+            }
 
             public TextView getTitleView() {
-                return titleView;
+                return this.titleView;
             }
+
+            public TextView getCreatedView() {
+                return this.createdView;
+            }
+
+            public TextView getCreatorView() {
+                return this.creatorView;
+            }
+
+            public TextView getAssigneeView() { return this.assigneeView; }
+
+            public void setExpandIcon(ImageView expandIcon) { this.expandIcon = expandIcon; }
+
+            public void setExpandPanel(RelativeLayout panel){
+                this.expandPanel = panel;
+            }
+
+            public void setBodyView(TextView bodyView) { this.bodyView = bodyView; }
 
             public void setTitleView(TextView titleView) {
                 this.titleView = titleView;
+            }
+
+            public void setCreatedView(TextView createdView) {
+                this.createdView = createdView;
+            }
+
+            public void setAssigneeView(TextView assigneeView) {
+                this.assigneeView = assigneeView;
+            }
+
+            public void setCreatorView(TextView creatorView) {
+                this.creatorView = creatorView;
             }
         }
     }
@@ -172,6 +288,7 @@ public class ListIssuesFragment extends ArnoldSupportFragment {
         issuesListView = (ListView) ret.findViewById(R.id.issue_list);
         listAdapter = new IssuesListAdapter();
         issuesListView.setAdapter(listAdapter);
+        issuesListView.setOnItemClickListener(this);
         return ret;
     }
 
