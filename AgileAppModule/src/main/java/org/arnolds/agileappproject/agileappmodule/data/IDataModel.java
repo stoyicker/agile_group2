af7c08b2608650e9c2df630f16286ab333bb9afa
Andreas Mathisen <andreas.per.mathisen@gmail.com>
2014-05-07 10:48:32 +0200

    Adds interface for storing events.

---
+++ AgileAppModule/src/main/java/org/arnolds/agileappproject/agileappmodule/data/IDataModel.java
@@ -0,0 +1,44 @@
+package org.arnolds.agileappproject.agileappmodule.data;
+
+
+import org.arnolds.agileappproject.agileappmodule.git.notifications.GitFile;
+import org.kohsuke.github.GHCommit;
+
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+public interface IDataModel {
+
+    /**
+     * Stores a commit that has been polled after the initial population.
+     * @param commit the commit that should be stored.
+     */
+    public void addLateCommit(final GHCommit commit);
+
+    /**
+     * Stores commits that has been polled after the initial population.
+     * @param commits the commits that should be stored.
+     */
+    public void addLateCommits(final List<GHCommit> commits);
+
+    /**
+     * Adds a commit with associated files that conflicts with the selected branch.
+     * @param commit - The commit that is associated with the file conflicts.
+     * @param affectedFiles - The files that are in conflict.
+     */
+    public void addFileConflict(final GHCommit commit, final List<GitFile> affectedFiles);
+
+    /**
+     * Adds a listener that should be added.
+     * Null is ignored and if a listener already exists it will now receive one additional event.
+     * @param listener the listener that should be added.
+     */
+    public void addPropertyChangeListener(final PropertyChangeListener listener);
+
+    /**
+     * Removes the specified listener.
+     * Null is ignored and if a listener exists multiple times it will now receive one event less.
+     * @param listener the lister that should be removed.
+     */
+    public void removePropertChangeListener(final PropertyChangeListener listener);
+}
